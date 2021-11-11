3fc322d9b3dc2b7fb17d96e98af053015c47693b
Aifedayo <akeemifedayolag@gmail.com>
2021-11-11 19:44:20 +0100

    models for pizza api

--- pizza/pizza_api/views.py
+++ pizza/pizza_api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Order, Customer, Pizza, CustomerAddress
+from .serializers import OrderSerializer, CustomerAddressSerializer, PizzaSerializer,CustomerSerializer
 
-# Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CustomerAddressViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomerAddressSerializer
+
+    def get_queryset(self):
+        customer_id = self.kwargs['customer_id']
+        return CustomerAddress.objects.filter(customer_id=customer_id)
+
+
+class PizzaViewSet(viewsets.ModelVieeSet):
+    queryset = Pizza.objects.filter()
+    serializer_class = PizzaSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        customer_id = self.kwargs['customer_id']
+        return Order.objects.filter(customer_id=customer_id)
