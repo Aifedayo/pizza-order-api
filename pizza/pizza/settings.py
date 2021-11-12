f579bbd6b143cb98ec5cfce6bb3ece9d2f439dcd
Aifedayo <akeemifedayolag@gmail.com>
2021-11-12 19:22:52 +0100

    models for pizza api

--- pizza/pizza/settings.py
+++ pizza/pizza/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': '127.0.0.1',
+        'PORT': '5433'
     }
 }
 
